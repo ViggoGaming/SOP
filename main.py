9b95bb0cded03ad82e053ce8bb4cd872e11fa65d
Victor Buch-Hansen <me@victorbuch.dk>
2022-12-16 12:39:53 +0100

    added graph to readme file

--- main.py
+++ main.py
@@ -85,6 +85,7 @@ def calculateNumberOfLines():
 
 
 def main():
+    start = False
     figure, axis = plt.subplots(2, 2)
     numberOflines: int = calculateNumberOfLines()
 
@@ -94,88 +95,97 @@ def main():
     csv_reader = [next(csv_reader) for x in range(numberOflines - 2)]
     random.shuffle(csv_reader)
 
-    lineCount = 0
-    for row in csv_reader:
-        # Opsætter de forskellige features
+    if st.button("Start KNN"):
+        st.write("KNN kører, vent venligst... ☕")
+        lineCount = 0
+        attackCount = 0
+        for row in csv_reader:
+            # Opsætter de forskellige features
 
-        # 0    1    2        3          4        5        6             7           8
-        # No.,Time,Source,Destination,Protocol,Length,Source port,Destination port,Type
-        source = float(row[2].replace(".", ""))
-        destination = float(row[3].replace(".", ""))
-        protocol = protocols.get(row[4])
-        length = int(row[5])
-        destinationPort = int(row[7])
-        attackType = attackTypes.get(row[8])
+            # 0    1    2        3          4        5        6             7           8
+            # No.,Time,Source,Destination,Protocol,Length,Source port,Destination port,Type
+            source = float(row[2].replace(".", ""))
+            destination = float(row[3].replace(".", ""))
+            protocol = protocols.get(row[4])
+            length = int(row[5])
+            destinationPort = int(row[7])
+            attackType = attackTypes.get(row[8])
 
-        point = Point(protocol, length, destinationPort, attackType)
-        # Put 1/3 of data in training data, and 2/3 in test data
-        if (lineCount % 3 == 0):
-            dataPoints.append(point)
-        else:
-            testPoints.append(point)
-        lineCount += 1
+            if attackType:
+                attackCount += 1
 
-    # Plotter alle datapointer
-    for data in dataPoints:
-        axis[1, 1].scatter(data.x, data.y, c=colors[data.label], marker="o")
+            point = Point(protocol, length, destinationPort, attackType)
+            # Put 1/3 of data in training data, and 2/3 in test data
+            if (lineCount % 3 == 0):
+                dataPoints.append(point)
+            else:
+                testPoints.append(point)
+            lineCount += 1
 
-    #    if data.label == 1:
-    #        print(data.x, data.y)
+        # Plotter alle datapointer
+        for data in dataPoints:
+            axis[1, 1].scatter(data.x, data.y, c=colors[data.label], marker="o")
 
-    for funcIndex in range(len(distFuncs)):
-        a = []
-        pa = None
-        for k in range(1, kMax):
-            print(f"k: {k}")
-            correct = 0
-            for testPoint in testPoints:
-                if KNN(testPoint.x, testPoint.y, testPoint.z, k, funcIndex) == testPoint.label:
-                    # axis[1,1].scatter(testPoint.x, testPoint.y, c=colors[testPoint.label], marker=matplotlib.markers.CARETUPBASE)
-                    correct += 1
-            a.append(correct / len(testPoints))
+        #    if data.label == 1:
+        #        print(data.x, data.y)
 
-        print(f"funcindex: {funcIndex}")
-        match funcIndex:
-            case 0:
-                axis[0, 0].plot([k for k in range(1, kMax)], a,
-                                "o-", color=colors[funcIndex])
-                axis[0, 0].set_title("(K, Præcision)-graf for DDOS-angreb")
-                axis[0, 0].legend("Euklid")
-                # axis[0, 0].set_xlabel="K"
-                # axis[0, 0].set_ylabel="Antal korrekte klassificeringer i %"
-                axis[0, 0].set_xticks([x for x in range(1, kMax)])
-            case 1:
-                axis[0, 1].plot([k for k in range(1, kMax)], a,
-                                "o-", color=colors[funcIndex])
-                axis[0, 1].set_title("(K, Præcision)-graf for DDOS-angreb")
-                axis[0, 1].legend("Manhattan")
-                # axis[0, 0].set_xlabel="K"
-                # axis[0, 0].set_ylabel="Antal korrekte klassificeringer i %"
-                axis[0, 1].set_xticks([x for x in range(1, kMax)])
-            case 2:
-                axis[1, 0].plot([k for k in range(1, kMax)], a,
-                                "o-", color=colors[funcIndex])
-                axis[1, 0].set_title("(K, Præcision)-graf for DDOS-angreb")
-                axis[1, 0].legend("Chebyshev")
-                # axis[0, 0].set_xlabel="K"
-                # axis[0, 0].set_ylabel="Antal korrekte klassificeringer i %"
-                axis[1, 0].set_xticks([x for x in range(1, kMax)])
+        for funcIndex in range(len(distFuncs)):
+            a = []
+            pa = None
+            for k in range(1, kMax):
+                print(f"k: {k}")
+                correct = 0
+                for testPoint in testPoints:
+                    if KNN(testPoint.x, testPoint.y, testPoint.z, k, funcIndex) == testPoint.label:
+                        # axis[1,1].scatter(testPoint.x, testPoint.y, c=colors[testPoint.label], marker=matplotlib.markers.CARETUPBASE)
+                        correct += 1
+                a.append(correct / len(testPoints))
 
-    figure.supxlabel("K")
-    figure.supylabel("Antal korrekte klassificeringer i %")
+            print(f"funcindex: {funcIndex}")
+            match funcIndex:
+                case 0:
+                    axis[0, 0].plot([k for k in range(1, kMax)], a,
+                                    "o-", color=colors[funcIndex])
+                    axis[0, 0].set_title("(K, Præcision)-graf for DDOS-angreb")
+                    axis[0, 0].legend("Euklid")
+                    # axis[0, 0].set_xlabel="K"
+                    # axis[0, 0].set_ylabel="Antal korrekte klassificeringer i %"
+                    axis[0, 0].set_xticks([x for x in range(1, kMax)])
+                case 1:
+                    axis[0, 1].plot([k for k in range(1, kMax)], a,
+                                    "o-", color=colors[funcIndex])
+                    axis[0, 1].set_title("(K, Præcision)-graf for DDOS-angreb")
+                    axis[0, 1].legend("Manhattan")
+                    # axis[0, 0].set_xlabel="K"
+                    # axis[0, 0].set_ylabel="Antal korrekte klassificeringer i %"
+                    axis[0, 1].set_xticks([x for x in range(1, kMax)])
+                case 2:
+                    axis[1, 0].plot([k for k in range(1, kMax)], a,
+                                    "o-", color=colors[funcIndex])
+                    axis[1, 0].set_title("(K, Præcision)-graf for DDOS-angreb")
+                    axis[1, 0].legend("Chebyshev")
+                    # axis[0, 0].set_xlabel="K"
+                    # axis[0, 0].set_ylabel="Antal korrekte klassificeringer i %"
+                    axis[1, 0].set_xticks([x for x in range(1, kMax)])
 
-#    st.pyplot(figure)
+        figure.supxlabel("K")
+        figure.supylabel("Antal korrekte klassificeringer i %")
 
-    figure = plt.gcf() # get current figure
-    figure.set_size_inches(19, 10)
-    # when saving, specify the DPI
-    plt.savefig("out.png", dpi = 100)
+    #    st.pyplot(figure)
 
-    image = Image.open("out.png")
-    st.image(image) 
+        figure = plt.gcf() # get current figure
+        figure.set_size_inches(19, 10)
+        # when saving, specify the DPI
+        plt.savefig("out.png", dpi = 100)
+
+        image = Image.open("out.png")
+        st.image(image) 
+
+        st.subheader(f"Der blev i alt fundet {attackCount} DDoS angreb i datasættet")
 
 
-inputFile = st.file_uploader("Upload en csv fil", type="csv")
+st.header("Victors IDS system ved brug af KNN")
+inputFile = st.file_uploader("Upload venligst dit datasæt", type="csv")
 
 if inputFile is not None:
     global file_contents
